0662e64bea0bdbe4e84448702beef67ea9182a90
LingETU <lucas.bondon@etu.univ-amu.fr>
2023-11-23 11:23:40 +0100

    init routing

--- angular-QRCODE-website/README.md
+++ angular-QRCODE-website/README.md
@@ -1,4 +1,4 @@
-# QRCODEAngularWebsite
+# AngularQRCODEWebsite
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.3.
 
--- angular-QRCODE-website/src/app/app.component.spec.ts
+++ angular-QRCODE-website/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'QRCODE-Angular-Website' title`, () => {
+  it(`should have the 'angular-QRCODE-website' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('QRCODE-Angular-Website');
+    expect(app.title).toEqual('angular-QRCODE-website');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, QRCODE-Angular-Website');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-QRCODE-website');
   });
 });
