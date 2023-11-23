0bff14167cd494d806c39688b8032dda2e12cb90
LingETU <lucas.bondon@etu.univ-amu.fr>
2023-11-23 11:12:31 +0100

    init push

---
+++ QRCODE-Angular-Website/src/app/qrcode/qrcode.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-qrcode',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './qrcode.component.html',
+  styleUrl: './qrcode.component.css'
+})
+export class QrcodeComponent {
+
+}
